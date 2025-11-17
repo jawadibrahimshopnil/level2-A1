c1ddd5115bb44f9a7d13423756a7f78c510b079f
jawadibrahim <jawadibrahimshopnil01@gmail.com>
2025-11-17 22:51:11 +0600

    modified pr0blem-1

--- solution.ts
+++ solution.ts
@@ -2,8 +2,5 @@
 function formatValue(input: (string | number | boolean)): string | number | boolean {
    if (typeof input === "string") return input.toUpperCase();
    if (typeof input === "number") return input*10;
-   if (typeof input === "boolean") return !input;
-
-   return "Unexpected type for pr0blem-1. It must be a string, number, or boolean, as given in the assignment.";
-} 
-
+   return !input;
+}
