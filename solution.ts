34015ed989f9ef46360e1833c99280a625b6dc61
jawadibrahim <jawadibrahimshopnil01@gmail.com>
2025-11-17 23:57:59 +0600

    add: pr0blem-2

--- solution.ts
+++ solution.ts
@@ -1,6 +1,16 @@
+type FormatValueT = (input: string | number | boolean) => (string | number | boolean);
 
-function formatValue(input: (string | number | boolean)): string | number | boolean {
+const formatValue: FormatValueT = (input) => {
    if (typeof input === "string") return input.toUpperCase();
    if (typeof input === "number") return input*10;
-   return !input;
+   if (typeof input === "boolean") return !input;
+   
+   throw new Error("Unexpected type for pr0blem-1. It must be string, number or boolean, as given in the assignment");
+}
+
+const getLength = (input: (string | any[])): number => {
+   if (typeof input === "string") return input.length;
+   if (Array.isArray(input)) return input.length;
+
+   throw new Error("Unexpected type for pr0blem-2. It must be string or array of any, as given in the assignment");
 }
