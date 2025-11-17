f52f3ef092c864f9aac4b01e5478ed5889192644
jawadibrahim <jawadibrahimshopnil01@gmail.com>
2025-11-17 22:46:07 +0600

    removed: pr0blem-1 console.log

--- solution.ts
+++ solution.ts
@@ -7,7 +7,3 @@ function formatValue(input: (string | number | boolean)): string | number | bool
    return "Unexpected type for pr0blem-1. It must be a string, number, or boolean, as given in the assignment.";
 } 
 
-console.log(formatValue('hello'));
-console.log(formatValue(5));
-console.log(formatValue(true));
-console.log(formatValue(false));
