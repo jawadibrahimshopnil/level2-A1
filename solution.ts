a897d1c32a72edf03d0494f4250d3ee90ade0a02
jawadibrahim <jawadibrahimshopnil01@gmail.com>
2025-11-18 11:18:23 +0600

    add: pr0blem-3

--- solution.ts
+++ solution.ts
@@ -5,12 +5,28 @@ const formatValue: FormatValueT = (input) => {
    if (typeof input === "number") return input*10;
    if (typeof input === "boolean") return !input;
    
-   throw new Error("Unexpected type for pr0blem-1. It must be string, number or boolean, as given in the assignment");
+   throw new Error("Unexpected type for pr0blem-1. It must be a string, number or boolean, as given in the assignment");
 }
 
 const getLength = (input: (string | any[])): number => {
    if (typeof input === "string") return input.length;
    if (Array.isArray(input)) return input.length;
 
-   throw new Error("Unexpected type for pr0blem-2. It must be string or array of any, as given in the assignment");
+   throw new Error("Unexpected type for pr0blem-2. It must be a string or an array of any, as given in the assignment");
 }
+
+class Person {
+   name:string;
+   age:number;
+
+   constructor(name:string, age:number){
+      this.name = name;
+      this.age = age;
+   }
+
+   getDetails():string{
+      return `'Name: ${this.name}, Age: ${this.age}'`
+   }
+
+}
+
